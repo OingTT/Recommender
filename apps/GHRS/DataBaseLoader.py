5baa4fa048ceecee890a9fe5a97b2a47c5fd6ee7
minjae <barrow13512@gmail.com>
2023-05-19 14:39:37 +0900

    DataBaseLoader fix

--- apps/GHRS/DataBaseLoader.py
+++ apps/GHRS/DataBaseLoader.py
@@ -128,35 +128,3 @@ class DataBaseLoader():
   
   def _test(self, args):
     print(self.__execute(*args))
-
-
-# def len_reviews_by_name(dbLoader: DataBaseLoader, userName):
-#   cnt = 0
-#   user = dbLoader.findUserByUserName(userName)
-#   uid = user[0][0]
-#   reviews = dbLoader.findReviesByUserId(uid)
-#   for row in reviews:
-#     if row[3] != 'WATCHED':
-#       continue
-#     cnt += 1
-#   print(f'{userName}: {cnt}')
-# def len_reviews_by_email(dbLoader: DataBaseLoader, userEmail):
-#   cnt = 0
-#   user = dbLoader.findUserByUserEmail(userEmail)
-#   userName = user[0][1]
-#   uid = user[0][0]
-#   reviews = dbLoader.findReviesByUserId(uid)
-#   for row in reviews:
-#     if row[3] != 'WATCHED':
-#       continue
-#     cnt += 1
-#   print(f'{userName}: {cnt}')
-
-
-# dbLoader = DataBaseLoader()
-# len_reviews_by_name(dbLoader, '박준서')
-# len_reviews_by_name(dbLoader, 'dong')
-# len_reviews_by_name(dbLoader, '이정준')
-# len_reviews_by_name(dbLoader, '김종인')
-# len_reviews_by_email(dbLoader, 'dalek76@naver.com')
-# len_reviews_by_name(dbLoader, '은댕')
